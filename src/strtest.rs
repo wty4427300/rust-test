3e919f9607a54fd86ffbfed46ceeae622bb5ed96
wty4427300 <wty4427300@gmail.com>
2020-11-04 18:59:44 +0800

    字符串相关

--- src/strtest.rs
+++ src/strtest.rs
@@ -5,5 +5,25 @@ fn test(){
 
     let i=data.to_string();
 
-    println!("{}",i)
+    println!("{}",i);
+
+    // let hello = String::from("السلام عليكم");
+    // let hello = String::from("Dobrý den");
+    // let hello = String::from("Hello");
+    // let hello = String::from("שָׁלוֹם");
+    // let hello = String::from("नमस्ते");
+    // let hello = String::from("こんにちは");
+    // let hello = String::from("안녕하세요");
+    // let hello = String::from("你好");
+    // let hello = String::from("Olá");
+    // let hello = String::from("Здравствуйте");
+    let mut hello = String::from("Hola");
+
+    hello.push_str("-insert");
+
+    println!("{}",hello);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
 }
