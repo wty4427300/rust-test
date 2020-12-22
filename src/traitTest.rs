d79949235a619495b4c20ae62aee6c3227d57269
wty4427300 <wty4427300@gmail.com>
2020-12-22 14:52:49 +0800

    玩一会rust的泛型

--- src/traitTest.rs
+++ src/traitTest.rs
@@ -1,7 +1,34 @@
+//定义一个泛型结构体,且下x,y是同一个类型
+struct Point<T>{
+    x:T,
+    y:T,
+}
+
+//定义一个泛型结构体,且下x,y是不同类型
+struct Point1<T,U>{
+    x:T,
+    y:U,
+}
+
+//枚举定义泛型
+enum Option<T>{
+    Some(T),
+    None,
+}
+
+impl <T> Point<T>{
+    fn x(&self) -> &T{
+        &self.x
+    }
+}
+
 fn trait_test(){
     let test=vec![10,20,30,40,66];
-    let result=largest(&test);
-    println!("The largest number is {}", result);
+    let result=largest_1(&test);
+    // println!("The largest number is {}", result);
+    //统一类型
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
 
 fn largest(list:&[i32])->i32{
@@ -14,3 +41,19 @@ fn largest(list:&[i32])->i32{
     }
     largest
 }
+
+//需要声明这个函数是一个泛形函数，返回值是一个泛形，参数也是一个泛形
+//报错的原因的是类型信息不足，或者是没有实现该类型的比较逻辑
+fn largest_1<T>(list:&T)->T{
+    let mut largest=list[0];
+
+    for &item in list.iter(){
+        if item>largest{
+            largest=item;
+        }
+    }
+    largest
+}
+
+
+
